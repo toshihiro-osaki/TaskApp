0700d1c3b98185a4cbd456dbb19ea1ea6ec6c0f8
toshihiro-osaki <lkjlkjozaki10>
2018-12-17 17:26:56 +0900

    taskapp課題提出

---
+++ TaskApp.java
@@ -0,0 +1,13 @@
+package jp.techacademy.osaki.toshihiro.taskapp;
+
+import android.app.Application;
+
+import io.realm.Realm;
+
+public class TaskApp extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        Realm.init(this);
+    }
+}
